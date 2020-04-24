7d291b6db6da79d723dddf3654ca56608db9a122
hannesthaler <02thahan@rgtfo-me.it>
2020-04-24 10:07:17 +0200

    Main

---
+++ Minirechner2/Minirechner2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie Zahl1 ein: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben sie Zahl2 ein: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+
+        }
+    }
+}
